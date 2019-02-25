d6691ed340603f0f0606471bbe16e7cece4911f5
Jaime Cernuda <jaime.cernuda95@gmail.com>
2019-02-25 01:38:17 -0500

    Some Bug Fixing

--- src/Transcation.py
+++ src/Transcation.py
@@ -81,10 +81,10 @@ class BlockChain:
 
     def check_block(self, block):
         previous_block = self.lookup_block_by_index(block.index - 1)
-        print("Hash (=)= " + block.hash[-3:] + " " + block.calculate_hash()[-3:])
-        print("Previous Hash (=) = " + block.previous_hash[-3:] + " " + previous_block.hash[-3:])
-        print("Index (>)= " + str(block.index) + " " + str(previous_block.index))
-        print("timestamp (>)= " + str(block.index) + " " + str(previous_block.timestamp))
+        print("Hash (=): " + block.hash[-3:] + " " + block.calculate_hash()[-3:])
+        print("Previous Hash (=): " + block.previous_hash[-3:] + " " + previous_block.hash[-3:])
+        print("Index (>): " + str(block.index) + " " + str(previous_block.index))
+        print("timestamp (>): " + str(block.timestamp) + " " + str(previous_block.timestamp))
 
         if (isinstance(block, Block) and previous_block.timestamp < block.timestamp and
                 block.index - previous_block.index == 1 and block.previous_hash == previous_block.hash and
